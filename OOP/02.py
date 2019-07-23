6b2dcaa49324c8ee91e1163c0574c1231306ebbc
SixNOstop <l>
2019-07-23 20:56:06 +0800

    No.2

---
+++ OOP/02.py
@@ -0,0 +1,20 @@
+
+class Student():
+    pass
+
+mingyue = Student()
+
+class PythonStudent():
+    name = None
+    age = 18
+    course = "Python"
+
+    def doHomework(self):
+        print("666")
+
+        return None
+
+yueyue = PythonStudent()
+print(yueyue.name)
+print(yueyue.age)
+yueyue.doHomework()
